6d71e162936f4ac024a9ff6127e16cc55eca1745
tetiana.zhydeikina.kn.2016@lpnu.ua <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-11-01 22:39:15 +0200

    add first version of model

---
+++ SciReg/Models/Article.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace SciReg.Models
+{
+    public class Article
+    {
+        public DateTime ReleaseDate { get; set; }
+        public String Title { get; set; }
+        public Subject ArticleSubject { get; set; }
+        public ContentType ArticleContentType { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public String ISSN { get; set; }
+        public Language ArticleLanguage { get; set; }
+        public String Text { get; set; }
+
+        //Author(User)
+        //Publisher(User)
+    }
+}
